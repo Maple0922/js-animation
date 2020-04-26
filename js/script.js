572f8d65cdef56776d4a46a449c63c96108a769e
Futo Nakajima <maple0922wh-142857@i.Softbank.jp>
2020-04-26 20:55:44 +0900

    2020/04/26 20:55

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
 $(function(){
-  // random polkadot
+  const spaceWidth = $('.main__space').innerWidth();
+  const spaceHeight = $('.main__space').innerHeight();
+
+  const randomInt = (m,n) => {
+    m = parseInt(m);
+    n = parseInt(n);
+    return Math.floor(Math.random()*(n-m+1)) + m;
+  }
+
+  const random = () => {
+    const randomLeft = randomInt(0,spaceWidth);
+    const randomTop = randomInt(0,spaceHeight);
+    return {left:randomLeft,top:randomTop};
+  }
+
+  // polkadot
+  const polkadot = $('.polkadot').children('.main__space');
+
+  setInterval(function(){
+    const randomPoint = random();
+    const dot = $('<p>');
+
+    dot.css({
+      'top': randomPoint.top,
+      'left': randomPoint.left,
+    });
+    polkadot.append(dot);
+
+    console.log('fot');
+
+    setTimeout(function(){
+      $('p').addClass('transform');
+    },10);
+
+  },10);
 });
