5ef3c4afbb0cad21f94b4a6eb78096dd4d3d1c29
Futo Nakajima <maple0922wh-142857@i.Softbank.jp>
2020-04-27 01:00:23 +0900

    2020/04/27 01:00

--- js/script.js
+++ js/script.js
@@ -27,8 +27,9 @@ $(function(){
   const polkadotSpace = $('.polkadot').children('.main__space');
 
   const polkadot = (size,fadeOutSecond,repeatSecond,darkness,lightness) => {
+    let i=0;
 
-    setInterval(function(){
+    let timerId = setInterval(function(){
       const random = randomValue(darkness,lightness);
       const dot = $('<p>');
 
@@ -51,12 +52,62 @@ $(function(){
         dot.remove();
       },fadeOutSecond);
 
+      i++;
+      if(i>20000/repeatSecond){
+        clearInterval(timerId);
+      }
+
     },repeatSecond);
+
   }
 
   $('.polkadot').children('.main__button').click(function(){
     // size,fadeOutSecond,repeatSecond,darkness,lightness
-    polkadot(200,5000,20,100,220);
+    polkadot(200,5000,30,100,220);
+  });
+
+
+  // rain
+
+  const rainSpace = $('.rain').children('.main__space');
+
+  const rain = (size,fadeOutSecond,repeatSecond) => {
+
+    let i=0;
+
+    let timerId = setInterval(function(){
+      const random = randomValue(null,null);
+      const dot = $('<p>');
+
+      dot.css({
+        'top': random.top,
+        'left': random.left,
+      });
+      rainSpace.append(dot);
+
+      dot.animate({
+        'width':size,
+        'height':size,
+        'opacity':'0',
+        'top': random.top-(size/2),
+        'left': random.left-(size/2),
+      },fadeOutSecond);
+
+      setTimeout(function(){
+        dot.remove();
+      },fadeOutSecond);
+
+      i++;
+      if(i>20000/repeatSecond){
+        clearInterval(timerId);
+      }
+
+    },repeatSecond);
+  }
+
+  $('.rain').children('.main__button').click(function(){
+    // size,fadeOutSecond,repeatSecond,darkness,lightness
+    rain(50,800,40,null,null);
   });
 
 });
